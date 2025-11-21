ce4f8aed274142db682d0e0b13e19fffe45db52c
EduardoLR10 <dudulr10@gmail.com>
2025-11-21 12:17:51 -0300

    Move Location module to a separate file

--- lib/Ast.ml
+++ lib/Ast.ml
@@ -1,26 +1,3 @@
-module Location = struct
-  include Lexing
-  open Lexing
-
-  type t = { pos_fname : string; pos_lnum : int; pos_bol : int; pos_cnum : int }
-  [@@deriving show, ord]
-
-  let to_position ({ pos_fname; pos_lnum; pos_bol; pos_cnum } : t) :
-      Lexing.position =
-    { pos_fname; pos_lnum; pos_bol; pos_cnum }
-
-  let to_t ({ pos_fname; pos_lnum; pos_bol; pos_cnum } : Lexing.position) : t =
-    { pos_fname; pos_lnum; pos_bol; pos_cnum }
-
-  type location = { startl : t; endl : t } [@@deriving show, ord]
-
-  type 'a with_location = { content : 'a; loc : location }
-  [@@deriving show, ord]
-
-  let add p1 p2 v = { content = v; loc = { startl = to_t p1; endl = to_t p2 } }
-  let strip_loc (v : 'a with_location) : 'a = v.content
-end
-
 type tag = string [@@deriving show, ord]
 
 type expr' = Variable of var | Functor of func | Integer of int
