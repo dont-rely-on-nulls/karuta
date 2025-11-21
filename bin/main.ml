f4d66623f43b03f17b00160f1cb3462273928479
EduardoLR10 <dudulr10@gmail.com>
2025-11-21 12:17:34 -0300

    Update files to proceed with compilation

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,14 @@
 open Cmd
 open Lib
 
+let show_clauses (clauses : Ast.clause list) : string =
+  let folder acc clause = acc ^ Ast.show_clause clause in
+  List.fold_left folder "" clauses
+
 let run : cmd -> unit = function
   | Compile { file } -> (
       match Executor.run file with
-      | None -> failwith @@ "Could not execute file: " ^ file
-      | Some computer ->
-          print_endline @@ Crawler.StandardOut.query_string computer)
+      | [] -> failwith @@ "Could not run file: " ^ file
+      | clauses -> print_endline @@ show_clauses clauses)
 
 let () = exit @@ parse_command_line_and_run run
--- lib/Executor.ml
+++ lib/Executor.ml
@@ -7,63 +7,66 @@ let parse (filepath : string) : Ast.parser_clause list =
   in
   filepath
   |> flip In_channel.with_open_text get_input
-  |> Option.map Parse.parse |> Option.to_list |> List.flatten
-  |> Parse.verify filepath
+  |> Option.map (Parse.parse filepath)
+  |> Option.to_list |> List.flatten |> Parse.verify filepath
 
-let compile' ((compiler, computer) : Compiler.t * Machine.t) :
-    Ast.parser_clause list -> Compiler.t * Machine.t = function
-  | [] ->
-      failwith "Compiler error: unreachable when executing compile function."
-  | decls_queries ->
-      (Preprocessor.group_clauses decls_queries, compiler, computer.store)
-      |> Compiler.compile
-      |> bimap Fun.id (Machine.update_store computer)
+let preprocess : Ast.parser_clause list -> Ast.clause list = function
+  | [] -> failwith "Preprocessor warning: empty clauses."
+  | decls_queries -> Preprocessor.group_clauses decls_queries
 
-let compile = compile' (Compiler.initialize (), Machine.initialize ())
+(* let compile' ((compiler, computer) : Compiler.t * Machine.t) : *)
+(*     Ast.parser_clause list -> Compiler.t * Machine.t = function *)
+(*   | [] -> *)
+(*       failwith "Compiler error: unreachable when executing compile function." *)
+(*   | decls_queries -> *)
+(*       (Preprocessor.group_clauses decls_queries, compiler, computer.store) *)
+(*       |> Compiler.compile *)
+(*       |> bimap Fun.id (Machine.update_store computer) *)
 
-let eval ((compiler, computer) : Compiler.t * Machine.t) :
-    (Compiler.t * Machine.t) option =
-  match compiler.entry_point with
-  | None -> None
-  | Some entry_point ->
-      let computer =
-        Evaluator.eval compiler.functor_table
-          { computer with p_register = entry_point.p_register }
-      in
-      Some (compiler, computer)
+(* let compile = compile' (Compiler.initialize (), Machine.initialize ()) *)
 
-let run (filepath : string) : Machine.t option =
-  filepath |> parse |> compile |> eval |> Option.map snd
+(* let eval ((compiler, computer) : Compiler.t * Machine.t) : *)
+(*     (Compiler.t * Machine.t) option = *)
+(*   match compiler.entry_point with *)
+(*   | None -> None *)
+(*   | Some entry_point -> *)
+(*       let computer = *)
+(*         Evaluator.eval compiler.functor_table *)
+(*           { computer with p_register = entry_point.p_register } *)
+(*       in *)
+(*       Some (compiler, computer) *)
 
-let load' filter_fn (filepath : string) : Compiler.t * Machine.t =
-  filepath |> parse |> List.filter filter_fn |> compile
+let run (filepath : string) : Ast.clause list = filepath |> parse |> preprocess
 
-let load = load' (Fun.const true)
-let load_decls = load' Ast.ParserClause.is_decl
+(* let load' filter_fn (filepath : string) : Compiler.t * Machine.t = *)
+(*   filepath |> parse |> List.filter filter_fn |> compile *)
 
-let load_many_decls : string list -> (Compiler.t * Machine.t) option = function
-  | [] -> None
-  | f :: fs ->
-      let get_decls filepath =
-        filepath |> parse
-        |> List.filter Ast.ParserClause.is_decl
-        |> BatFingerTree.of_list
-      in
-      fs
-      |> List.fold_left
-           (fun acc f' -> BatFingerTree.append acc @@ get_decls f')
-           (get_decls f)
-      |> BatFingerTree.to_list |> compile |> Option.some
+(* let load = load' (Fun.const true) *)
+(* let load_decls = load' Ast.ParserClause.is_decl *)
 
-let continue content compiler_and_computer : (Compiler.t * Machine.t) option =
-  match (Parse.parse content, compiler_and_computer) with
-  | [], _ ->
-      print_endline ("Parser error. Incorrect definition: " ^ content);
-      None
-  | decls_queries, Some (compiler, computer) ->
-      decls_queries
-      |> compile'
-           ( { compiler with entry_point = None },
-             { computer with query_variables = BatMap.empty } )
-      |> eval
-  | decls_queries, None -> decls_queries |> compile |> eval
+(* let load_many_decls : string list -> (Compiler.t * Machine.t) option = function *)
+(*   | [] -> None *)
+(*   | f :: fs -> *)
+(*       let get_decls filepath = *)
+(*         filepath |> parse *)
+(*         |> List.filter Ast.ParserClause.is_decl *)
+(*         |> BatFingerTree.of_list *)
+(*       in *)
+(*       fs *)
+(*       |> List.fold_left *)
+(*            (fun acc f' -> BatFingerTree.append acc @@ get_decls f') *)
+(*            (get_decls f) *)
+(*       |> BatFingerTree.to_list |> compile |> Option.some *)
+
+(* let continue content compiler_and_computer : (Compiler.t * Machine.t) option = *)
+(*   match (Parse.parse content, compiler_and_computer) with *)
+(*   | [], _ -> *)
+(*       print_endline ("Parser error. Incorrect definition: " ^ content); *)
+(*       None *)
+(*   | decls_queries, Some (compiler, computer) -> *)
+(*       decls_queries *)
+(*       |> compile' *)
+(*            ( { compiler with entry_point = None }, *)
+(*              { computer with query_variables = BatMap.empty } ) *)
+(*       |> eval *)
+(*   | decls_queries, None -> decls_queries |> compile |> eval *)
