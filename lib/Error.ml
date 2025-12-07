7632e029f49196ad4282fce780372263c002e074
EduardoLR10 <dudulr10@gmail.com>
2025-12-06 22:26:38 -0300

    Add more errors for handling

--- lib/Error.ml
+++ lib/Error.ml
@@ -1,15 +1,27 @@
 open Result
 
-type t = CouldNotPreprocess of string | CouldNotParse of string | InvalidFile
+type t =
+  | CouldNotPreprocess of string
+  | EmptyFilepath
+  | EmptyFile of string
+  | InvalidExtension of string
 [@@deriving show, ord]
 
 let display : t -> string = function
   | CouldNotPreprocess filepath -> "Could not preprocess file " ^ filepath
-  | CouldNotParse filepath -> "Could not parse file" ^ filepath
-  | InvalidFile -> "Input file is empty"
+  | EmptyFilepath -> "Input filepath is empty"
+  | EmptyFile file -> "Input file '" ^ file ^ "' is empty"
+  | InvalidExtension file -> "Input file '" ^ file ^ "' has invalid extension"
 
 type 'a attempt = ('a, t) result [@@deriving show, ord]
 
 let ok = ok
 let error = error
 let ( ||> ) = bind
+
+let ( let* ) o f =
+  match o with
+  | Error err ->
+      Logger.simply_error @@ display err;
+      exit 1
+  | Ok x -> f x
