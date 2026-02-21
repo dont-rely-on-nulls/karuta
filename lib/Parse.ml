10fc9787a694042d17e78156b249d26ec470cb0c
Marinho <marinho.pinotti@gmail.com>
2026-02-21 00:01:06 -0300

    Nuke the old parser

--- lib/Parse.ml
+++ lib/Parse.ml
@@ -1,49 +1,3 @@
-open Lexing
-module E = MenhirLib.ErrorReports
-module L = MenhirLib.LexerUtil
-module I = Parser.MenhirInterpreter
-
-let env checkpoint =
-  match checkpoint with
-  | I.HandlingError env -> env
-  | _ ->
-      Logger.simply_unreachable "env: parser is broken";
-      assert false
-
-let state = Fun.compose I.current_state_number env
-
-let get input checkpoint i =
-  match I.get i (env checkpoint) with
-  | Some (I.Element (_, _, pos1, pos2)) ->
-      (pos1, pos2) |> E.extract input |> E.sanitize |> E.compress
-  | None ->
-      Logger.simply_unreachable "get: parser is broken";
-      exit 1
-
-let fail input buffer (checkpoint : _ I.checkpoint) =
-  let pos1, pos2 = E.last buffer in
-  let open Location in
-  let loc = { startl = to_t pos1; endl = to_t pos2 } in
-  let msg = ParserMessages.message (state checkpoint) in
-  let msg = E.expand (get input checkpoint) msg in
-  Logger.error loc msg;
-  exit 1
-
-let parse_loop (input : string) (lexbuf : lexbuf) =
-  let supplier = I.lexer_lexbuf_to_supplier Lexer.read lexbuf in
-  let buffer, supplier = E.wrap_supplier supplier in
-  let checkpoint = Parser.Incremental.file lexbuf.lex_curr_p in
-  I.loop_handle Fun.id (fail input buffer) supplier checkpoint
-
-let parse (filepath : string) : Ast.ParserClause.t list =
-  let input, lexbuf = L.read filepath in
-  try parse_loop input lexbuf
-  with Lexer.SyntaxError (loc, message) ->
-    Logger.error loc message;
-    exit 1
-
-(* New Parser *)
-
 type parser_state = { remaining : BatSubstring.t; loc : Location.t }
 type ('a, 'e) parser = parser_state -> ('a * parser_state, 'e) result
 
@@ -503,7 +457,7 @@ and top_level : 'e. (Ast.ParserClause.t list, ([> expr_errors ] as 'e)) parser =
   |> plus (skip_whitespace_and_comments @&& parser_clause)
      @> replace @@ BatFingerTree.to_list
 
-let parse' (filepath : string) (source : string) =
+let parse (filepath : string) (source : string) =
   match
     {
       remaining = BatSubstring.all source;
