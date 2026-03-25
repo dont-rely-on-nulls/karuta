59e1024cf77fd927fe2269a7a6c73db01bc0f199
EduardoLR10 <dudulr10@gmail.com>
2026-03-25 20:05:09 -0300

    Implement serialization for query in Sakura

--- runtime/sakura.erl
+++ runtime/sakura.erl
@@ -4,7 +4,51 @@
 
 -include("sakura.hrl").
 
-serialize(Query) -> Query. % TODO
+serialize_primitive(Primitive) when is_binary(Primitive)-> ["\"", Primitive, "\""];
+serialize_primitive(Primitive) when is_integer(Primitive)-> integer_to_list(Primitive);
+serialize_primitive(Primitive) when is_float(Primitive)-> float_to_list(Primitive);
+serialize_primitive(Primitive) when is_boolean(Primitive)-> atom_to_list(Primitive).
+
+serialize_tag_value(TagValue) ->
+    case TagValue of
+        int -> "Int";
+        float -> "Float";
+        str -> "Str";
+        bool -> "Bool"
+    end.
+
+serialize_value({Tag, Primitive}) ->
+    ["(", serialize_tag_value(Tag), " ", serialize_primitive(Primitive), ")"].
+
+serialize_symbol(Key) when is_binary(Key) -> serialize_primitive(Key);
+serialize_symbol(Key) when is_atom(Key) -> atom_to_list(Key).
+
+serialize_map(Map, SerializeValue) ->
+  [maps:fold(
+    fun (Key, Value, Acc) -> 
+            SerializedKey = serialize_symbol(Key),
+            SerializedValue = SerializeValue(Value),
+            [Acc, "(", SerializedKey, " ", SerializedValue,")"]
+    end, "(", Map), ")"].
+
+serialize_list_of_symbols(ListOfSymbols) -> lists:map(fun serialize_symbol/1, ListOfSymbols).
+
+serialize(Query) -> 
+    case Query of
+        {base, RelationName}     -> ["(Base ", serialize_symbol(RelationName), ")"];
+        {const, Const}           -> ["(Const ", serialize_map(Const, fun serialize_value/1), " )"]; 
+        {select, Query1, Query2} -> ["(Select ", serialize(Query1), " ", serialize(Query2), ")"];
+        {join, ListOfSymbols, Query1, Query2} -> 
+            ["(Join ", serialize_list_of_symbols(ListOfSymbols), " ", 
+             serialize(Query1), " ", serialize(Query2), ")"];
+        {cartesian, Query1, Query2} -> ["(Cartesian ", serialize(Query1), " ", serialize(Query2), ")"];
+        {project, ListOfSymbols, Query1} -> ["(Project ", serialize_list_of_symbols(ListOfSymbols), " ", serialize(Query1), ")"];
+        {rename, RenameMap, Query1} -> ["(Rename ", serialize_map(RenameMap, fun serialize_symbol/1), " ", serialize(Query1), " )"]; 
+        {union, Query1, Query2} -> ["(Union ", serialize(Query1), " ", serialize(Query2), ")"];
+        {diff, Query1, Query2} -> ["(Diff ", serialize(Query1), " ", serialize(Query2), ")"];
+        {take, HowMany, Query1} -> ["(Take ", serialize_primitive(HowMany), " ", serialize(Query1), ")"]
+    end.
+
 deserialize(Response) -> {ok, Response}. % TODO
 create_session(_Socket, _Query) -> {ok, dummy}. % TODO
 
--- runtime/sakura.hrl
+++ runtime/sakura.hrl
@@ -1,13 +1,15 @@
 -type primitive() :: binary() | integer() | float() | boolean().
 
+-type symbol() :: binary() | atom().
+
 -type query() ::
-    {base, binary()}                           % base relation by name
-  | {const, #{binary() => primitive()}}        % constant single-tuple relation
-  | {select, query(), query()}                 % σ semijoin: (filter, source)
-  | {join, [binary()], query(), query()}       % ⋈ natural equijoin on named attrs
-  | {cartesian, query(), query()}              % × Cartesian product
-  | {project, [binary()], query()}             % π restrict columns
-  | {rename, #{binary() => binary()}, query()} % ρ rename (old,new) pairs
-  | {union, query(), query()}                  % ∪ — compatible schemas assumed
-  | {diff, query(), query()}                   % −
-  | {take, integer(), query()}.                % τ
+    {base, symbol()}                               % base relation by name
+  | {const, #{symbol() => primitive()}} % constant single-tuple relation
+  | {select, query(), query()}                     % σ semijoin: (filter, source)
+  | {join, [symbol()], query(), query()}           % ⋈ natural equijoin on named attrs
+  | {cartesian, query(), query()}                  % × Cartesian product
+  | {project, [symbol()], query()}                 % π restrict columns
+  | {rename, #{symbol() => symbol()}, query()}     % ρ rename (old,new) pairs
+  | {union, query(), query()}                      % ∪ — compatible schemas assumed
+  | {diff, query(), query()}                       % −
+  | {take, integer(), query()}.                    % τ
