d94f0772ad1a01b91630a92b9435bd292e3484c6
Marinho <marinho.pinotti@gmail.com>
2026-09-18 22:17:17 -0300

    Isolate test cases from each other
    Also isolate compilation from running so we can give different error
    messages for each failure mode.

--- test/runner.ml
+++ test/runner.ml
@@ -1,42 +1,79 @@
 type t = {
-  name : string;
   filepaths : string list;
   root_module : string;
   expected : string;
   limit : int option;
 }
 
-let examples : t list =
-  [
-    {
-      name = "plus";
-      filepaths = [ "arithmetic/plus.krt" ];
-      root_module = "plus";
-      limit = Some 2;
-      expected = "---------\nLHS = 0\nRHS = 6\n---------\nLHS = -1\nRHS = 7\n";
-    };
-  ]
+let examples : t BatMap.String.t =
+  BatMap.String.of_list
+    [
+      ( "plus",
+        {
+          filepaths = [ "arithmetic/plus.krt" ];
+          root_module = "plus";
+          limit = Some 2;
+          expected =
+            "---------\nLHS = 0\nRHS = 6\n---------\nLHS = -1\nRHS = 7\n";
+        } );
+      ( "minus",
+        {
+          filepaths = [ "arithmetic/minus.krt" ];
+          root_module = "minus";
+          limit = Some 2;
+          expected =
+            "---------\nLHS = 0\nRHS = 0\n---------\nLHS = -1\nRHS = -1\n";
+        } );
+    ]
 
-let make_test { name; expected; limit; root_module; filepaths } =
-  Unix.chdir "../runtime";
-  let prefix file = "../examples/" ^ file in
-  match
-    Lib.Executor.run ~limit
-      { filename = "./play"; root_module }
-      {
-        beam =
-          (fun name forms ->
-            Bin.Erl.compile "" name @@ BatFingerTree.to_list forms);
-        executable = Lib.Executable.create_file;
-      }
-      (List.map prefix filepaths)
-  with
-  | Error _ -> (name, Alcotest.fail ("Could not run example: " ^ name))
-  | Ok output ->
-      ( name,
+let make_test current_directory
+    (name, { expected; limit; root_module; filepaths }) =
+  (try Unix.mkdir name 0o766 with Unix.Unix_error (Unix.EEXIST, _, _) -> ());
+  (try Unix.link "../runtime/karuta.beam" (name ^ "/karuta.beam")
+   with Unix.Unix_error (Unix.EEXIST, _, _) -> ());
+  ( name,
+    let test_directory = current_directory ^ "/" ^ name in
+    let prefix file = current_directory ^ "/../examples/" ^ file in
+    let filename = test_directory ^ "/play" in
+    let open Lib.Error in
+    match
+      Lib.Executor.compile
+        {
+          artifact =
+            Lib.Shared.Compiler.Options.Executable { filename; root_module };
+          sakura = None;
+        }
+        {
+          beam =
+            (fun name forms ->
+              Bin.Erl.compile test_directory name @@ BatFingerTree.to_list forms);
+          executable = Lib.Executable.create_file;
+        }
+        (List.map prefix filepaths)
+    with
+    | Error e ->
+        Alcotest.fail
+          ("Could not compile example: " ^ name ^ " (" ^ display e ^ ")")
+    | Ok () ->
         [
           ( Alcotest.test_case name `Quick @@ fun () ->
-            Alcotest.(check string) ("Ran " ^ name) expected output );
+            Unix.chdir test_directory;
+            match
+              Lib.Posix.run_process
+                (match limit with
+                | None -> [| filename |]
+                | Some n -> [| filename; string_of_int n |])
+            with
+            | Error e ->
+                Alcotest.fail
+                  ("Could not run example: " ^ name ^ " ("
+                 ^ process_status_message e ^ ")")
+            | Ok output ->
+                Alcotest.(check string) ("Ran " ^ name) expected output );
         ] )
 
-let () = Alcotest.run "Examples" (List.map make_test examples)
+let () =
+  Alcotest.run "Examples"
+    (BatMap.String.to_seq examples
+    |> BatSeq.map (make_test (Unix.getcwd ()))
+    |> BatList.of_seq)
